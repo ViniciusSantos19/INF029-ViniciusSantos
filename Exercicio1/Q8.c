9eb0050c89da5be569807ed6c2bb2620a16ff5d5
Vinicius Rodrigues <93113315+ViniciusSantos19@users.noreply.github.com>
2022-03-01 01:45:59 +0000

    adicionado lsita de funcoes

---
+++ Exercicio1/Q8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 5
+int main()
+{
+  int funcaomult(int);
+  
+ }
+int funcaomulti(int a, int b){
+  printf("Digite o valor de a: \n");
+   scanf("%d",&a);
+  printf("Digite o valor de b: \n");
+  scanf("%d",&b);
+  int num = a * b;
+  return num;
+}
+
+ 
+
---
+++ Lista1/Q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int soma(int a1, int b1){
+  int soma;
+  soma = a1 + b1;
+  return soma;
+}
+int main()
+{
+ int num1 num2;
+ int resultado;
+ printf("Digite um número: \n");
+  scanf("%d",&num1);
+   printf("Digite um número: \n");
+  scanf("%d",&num2);
+  resultado= soma(num1, num2);
+}
